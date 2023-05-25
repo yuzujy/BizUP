a946c72a04921fa56f7cd7c9f1bee17e7c6cab94
yuzujy <e0968312@u.nus.edu>
2023-05-25 18:53:28 +0800

    adding files

---
+++ app/profile.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const profile = () => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text> 
+                profile
+            </Text>
+        </View>
+    );
+};
+
+export default profile;
