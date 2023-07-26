e5df94e43566b330c53bd7852f3d29fd45230aa8
junglebollz <e0967946@u.nus.edu>
2023-07-27 06:07:12 +0800

    Add appointment booking files to File-Upload

---
+++ BizUP-main/components/Button.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { TouchableOpacity, Text, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import { COLORS, SIZES, FONTS, SHADOWS } from "../constants";
+
+export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        width: 40,
+        height: 40,
+        backgroundColor: COLORS.white,
+        position: "absolute",
+        borderRadius: SIZES.extraLarge,
+        alignItems: "center",
+        justifyContent: "center",
+        ...SHADOWS.light,
+        ...props,
+      }}
+      onPress={handlePress}
+    >
+      <Image
+        source={imgUrl}
+        resizeMode="contain"
+        style={{ width: 24, height: 24 }}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export const MenuButton = ({ iconUrl, handlePress }) => {
+  return (
+    <TouchableOpacity style={{ width: 45, height: 45 }} onPress={handlePress}>
+      <Image
+        source={iconUrl}
+        resizeMode="cover"
+        style={{ width: "100%", height: "100%" }}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export const RectButton = ({ minWidth, fontSize, ...props }) => {
+  const navigation = useNavigation();
+
+  const navigateToAppointmentSlots = () => {
+    navigation.navigate('apptSlots');
+  };
+
+  return (
+    <TouchableOpacity
+      style={{
+        backgroundColor: COLORS.primary,
+        padding: SIZES.small,
+        borderRadius: SIZES.extraLarge,
+        minWidth: minWidth,
+        ...props,
+      }}
+      onPress={navigateToAppointmentSlots}
+    >
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: fontSize,
+          color: COLORS.white,
+          textAlign: "center",
+        }}
+      >
+        Book Now
+      </Text>
+    </TouchableOpacity>
+  );
+};
---
+++ BizUP-main/constants/icons.js
@@ -0,0 +1,20 @@
+import menu from "../assets/icons/menu.png";
+import filter from "../assets/icons/filter.png";
+import heartOutline from "../assets/icons/heart-ol.png";
+import share from "../assets/icons/share.png";
+import location from "../assets/icons/location.png";
+import chevronLeft from '../assets/icons/chevron-left.png'
+import chevronRight from '../assets/icons/chevron-right.png';
+import left from "../assets/icons/left.png";
+
+export default {
+  menu,
+  filter,
+  left,
+  heartOutline,
+  share,
+  location,
+  chevronLeft,
+  chevronRight
+};
+
